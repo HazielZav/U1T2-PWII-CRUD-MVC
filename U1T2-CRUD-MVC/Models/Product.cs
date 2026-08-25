69b213448b5ebcd294289d72f042fd4e8d4d7bba
Haziel Zavala <kevlinlal@gmail.com>
2026-08-24 21:45:58 -0600

    Añadidas propiedades y mejoras de visualización en Product
    Se ha añadido el atributo `[Display(Name = "Nombre del producto")]`
    a la propiedad `ProductName` para mejorar su presentación.
    
    Se han agregado nuevas propiedades con sus respectivos atributos
    `[Display(Name = "...")]` para enriquecer la interfaz de usuario:
    - `SupplierId`, `CategoryId`, `QuantityPerUnit`, `UnitPrice`,
      `UnitsInStock`, `UnitsOnOrder`, y `ReorderLevel`.
    
    Se ha añadido la propiedad `Discontinued` para indicar si un
    producto está descontinuado.
    
    Se ha añadido la propiedad calculada `Estado`, que devuelve
    "Descontinuado" o "Activo" según el valor de `Discontinued`.

--- U1T2-CRUD-MVC/Models/Product.cs
+++ U1T2-CRUD-MVC/Models/Product.cs
@@ -10,23 +10,34 @@ public partial class Product
 
     [Required(ErrorMessage = "El nombre del producto es obligatorio.")]
     [StringLength(40, MinimumLength = 2, ErrorMessage = "El nombre de producto debe estar entre 2 y 40 caracteres.")]
+    [Display(Name ="Nombre del producto")]
     public string ProductName { get; set; } = null!;
 
+    [Display(Name = "Proveedor")]
     public int? SupplierId { get; set; }
 
+    [Display(Name = "Categoría")]
     public int? CategoryId { get; set; }
 
+    [Display(Name = "Cantidad por unidad")]
     public string? QuantityPerUnit { get; set; }
 
+    [Display(Name = "Precio unitario")]
     public decimal? UnitPrice { get; set; }
 
+    [Display(Name = "Unidades en stock")]
     public short? UnitsInStock { get; set; }
 
+    [Display(Name = "Unidades en orden")]
     public short? UnitsOnOrder { get; set; }
 
+    [Display(Name = "Nivel de reorden")]
     public short? ReorderLevel { get; set; }
 
+    
     public ulong Discontinued { get; set; }
+    [Display(Name = "Estado")]
+    public string Estado => Discontinued == 1 ? "Descontinuado" : "Activo";
 
     public virtual Category? Category { get; set; }
 
