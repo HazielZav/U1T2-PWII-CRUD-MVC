7a8a66c33a10b61bdf1a014cf03aafbb8b76c37c
hazielzav <kevlin@gmail.com>
2026-09-09 17:48:09 -0600

    Refactorización y adición de controladores y vistas CRUD
    Se realizaron los siguientes cambios principales:
    
    - Se movió el método `ConsumoClienteVista` de `ProductsController` a `CustomersController`, añadiendo lógica para agrupar y ordenar datos.
    - Se reubicaron las vistas `ConsumoClienteVista` en la carpeta `Views/Customers`.
    - Se agregó el controlador `CustomersController` con acciones CRUD y soporte para `ConsumoClienteVista`.
    - Se añadió el controlador `EmployeesController` con acciones CRUD y manejo de relaciones jerárquicas.
    - Se crearon vistas CRUD para `Customers` y `Employees` en sus respectivas carpetas.
    - Se implementaron etiquetas Razor en las vistas para formularios y enlaces dinámicos.
    - Se añadieron validaciones y lógica para mostrar datos de las entidades `Customer` y `Employee`.

---
+++ U1T2-CRUD-MVC/Controllers/CustomersController.cs
@@ -0,0 +1,181 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using U1T2_CRUD_MVC.Data;
+using U1T2_CRUD_MVC.Models;
+
+namespace U1T2_CRUD_MVC.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly ContextoBD _context;
+
+        public CustomersController(ContextoBD context)
+        {
+            _context = context;
+        }
+
+        // GET: Customers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Customers.ToListAsync());
+        }
+
+        // GET: Customers/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers
+                .FirstOrDefaultAsync(m => m.CustomerId == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // GET: Customers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CustomerId,CompanyName,ContactName,ContactTitle,Address,City,Region,PostalCode,Country,Phone,Fax")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(customer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("CustomerId,CompanyName,ContactName,ContactTitle,Address,City,Region,PostalCode,Country,Phone,Fax")] Customer customer)
+        {
+            if (id != customer.CustomerId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(customer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CustomerExists(customer.CustomerId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers
+                .FirstOrDefaultAsync(m => m.CustomerId == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer != null)
+            {
+                _context.Customers.Remove(customer);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CustomerExists(string id)
+        {
+            return _context.Customers.Any(e => e.CustomerId == id);
+        }
+
+        public async Task<IActionResult> ConsumoClienteVista()
+        {
+            var contextoBD = _context.Orderdetails.Include(od => od.Order)
+                .ThenInclude(o => o.Customer)
+                .Include(od => od.Product)
+                .Select((od) => new ConsumoCliente()
+                {
+                    Cliente = od.Order.Customer.CompanyName,
+                    Producto = od.Product.ProductName,
+                    Cantidad = od.Quantity
+                })
+                .GroupBy(x => new { x.Cliente, x.Producto })
+                .Select(g => new ConsumoCliente()
+                {
+                    Cliente = g.Key.Cliente,
+                    Producto = g.Key.Producto,
+                    Cantidad = g.Sum(x => x.Cantidad)
+                })
+                .OrderBy(c => c.Cliente)
+                .ThenByDescending(c => c.Cantidad)
+                .ThenBy(C => C.Producto);
+            return View(await contextoBD.ToListAsync());
+        }
+    }
+}
--- U1T2-CRUD-MVC/Views/Customers/ConsumoClienteVista.cshtml.cs
+++ U1T2-CRUD-MVC/Views/Customers/ConsumoClienteVista.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace U1T2_CRUD_MVC.Views.Products
+namespace U1T2_CRUD_MVC.Views.Customers
 {
     public class Index2Model : PageModel
     {
